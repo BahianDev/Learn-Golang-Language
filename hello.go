49a2aa1dfea0c7e5748f0f68d07dd249d9e5333f
wesley <wesleysilvadesousa@gmail.com>
2020-04-26 21:54:57 -0300

    for and if statements

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 
 )
@@ -29,8 +30,22 @@ func split(sum int) (x, y int){
 	return
 }
 
+func sqrt(x float64) string{
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+
+func pow (x, n, lim float64) float64{
+	if v := math.Pow(x,n); v < lim{
+		return v 
+	}
+	return lim
+}
 func main(){
-	fmt.Printf("Hello, world 123\n")
+	fmt.Printf("Hello, world 1234\n")
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Println(add(2,2))
 
@@ -44,10 +59,26 @@ func main(){
 		sum += i
 	}
 	fmt.Println(sum)
-	
-	sum=1
+
+		sum=1
 	for ; sum < 100;{
 		sum += sum
 	}
 	fmt.Println(sum)
+
+	//For continued
+	sum=1 
+	for ; sum < 100; {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+	//Infinite Loop
+	//for{
+	//}
+	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(
+		pow(3,2,10),
+		pow(3,3,20),
+	)
 }
